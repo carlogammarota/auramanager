b949df8a09e054ef003fb124f8dfe335e1a4c52c
Carlo Gammarota <carlo.gammarota@gmail.com>
2023-12-22 15:27:03 -0300

    server

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const history = require('connect-history-api-fallback');
+
+const app = express();
+
+// Manejo del modo History para Vue.js Router
+app.use(history());
+
+// Sirviendo archivos estáticos desde la carpeta 'dist'
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Ruta raíz, sirviendo el archivo index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+// Iniciar el servidor
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
