64881fb8863606455b2022711be8ed5182c03f71
ubuntu <carlo.gammarota@gmail.com>
2025-01-14 18:42:02 -0300

    responsive

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,82 +1,81 @@
 <template>
-    <div>
-      <!-- Navbar -->
-<nav class="bg-gray-800 p-4 flex justify-between items-center flex-wrap  mx-auto">
-  <div class="flex items-center space-x-2 text-2xl font-bold">
-    <img src="../assets/auraBlanco.png" class="w-32 " alt="Profile Picture">
-    <!-- <span class="hidden sm:block" style="margin-bottom: 13px;">Aura Producciones</span> -->
-  </div>
-
-  <!-- Menu de navegación -->
-  <div class="flex items-center space-x-4 sm:space-x-6">
-
-    <!-- User Profile Login -->
-    <div class="flex items-center space-x-3" v-if="getUser.name">
-      <!-- {{ getUser }} -->
-      <p class="text-lg font-medium text-white hidden md:block" >{{ getUser.name }}</p>
-      <img :src="getUser.imagen" id="user_picture"
-       class="w-10 h-10 rounded-full object-cover" alt="Profile Picture">
-    </div>
-
-    <!-- no login -->
-    <div class="flex justify-center" v-if="!getUser.name">
-      <div class="">
-        <a href="https://api-aura.armortemplate.com/auth/google"
-        class="flex items-center px-4 py-2 bg-white text-gray-700 font-medium border border-gray-300 rounded-lg shadow-sm hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/48px-Google_%22G%22_logo.svg.png?20230822192911"
-          alt="Google" class="w-5 h-5 mr-2">
-        Entrar con Google
-      </a>
+  <div>
+    <!-- Navbar -->
+    <nav class="bg-gray-800 p-4 flex justify-between items-center flex-wrap  mx-auto">
+      <div class="flex items-center space-x-2 text-2xl font-bold">
+        <img src="../assets/auraBlanco.png" class="w-32 " alt="Profile Picture">
+        <!-- <span class="hidden sm:block" style="margin-bottom: 13px;">Aura Producciones</span> -->
       </div>
-    </div>
 
-    <!-- Icono de menú para pantallas pequeñas -->
-    <div class="sm:hidden">
-      <button id="menuToggle" class="text-white focus:outline-none">
-        <i class="fas fa-bars text-2xl"></i>
-      </button>
-    </div>
+      <!-- Menu de navegación -->
+      <div class="flex items-center space-x-4 sm:space-x-6">
+
+        <!-- User Profile Login -->
+        <div class="flex items-center space-x-3" v-if="getUser.name">
+          <!-- {{ getUser }} -->
+          <p class="text-lg font-medium text-white hidden md:block">{{ getUser.name }}</p>
+          <img :src="getUser.imagen" id="user_picture" class="w-10 h-10 rounded-full object-cover"
+            alt="Profile Picture">
+        </div>
+
+        <!-- no login -->
+        <div class="flex justify-center" v-if="!getUser.name">
+          <div class="">
+            <a href="https://api-aura.armortemplate.com/auth/google"
+              class="flex items-center px-4 py-2 bg-white text-gray-700 font-medium border border-gray-300 rounded-lg shadow-sm hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/48px-Google_%22G%22_logo.svg.png?20230822192911"
+                alt="Google" class="w-5 h-5 mr-2">
+              Entrar con Google
+            </a>
+          </div>
+        </div>
+
+        <!-- Icono de menú para pantallas pequeñas -->
+        <div class="sm:hidden">
+          <button id="menuToggle" class="text-white focus:outline-none">
+            <i class="fas fa-bars text-2xl"></i>
+          </button>
+        </div>
+      </div>
+
+
+
+      <!-- Menú desplegable para pantallas pequeñas -->
+      <div id="mobileMenu"
+        class="w-full hidden sm:hidden flex-col items-center space-y-4 p-4 bg-gray-800 text-white sm:hidden">
+        <div class="flex items-center space-x-3">
+          Menu
+        </div>
+
+      </div>
+    </nav>
+    <main>
+
+      <router-view />
+    </main>
   </div>
+</template>
 
-  
-
-  <!-- Menú desplegable para pantallas pequeñas -->
-  <div id="mobileMenu"
-    class="w-full hidden sm:hidden flex-col items-center space-y-4 p-4 bg-gray-800 text-white sm:hidden">
-    <div class="flex items-center space-x-3">
-      Menu
-    </div>
-
-  </div>
-</nav>
-      <main>
-        
-        <router-view />
-      </main>
-    </div>
-  </template>
-  
-  <script>
-    import { mapGetters, mapActions } from "vuex";
-  export default {
-    name: "SiteLayout",
-    computed: {
-      getToken() {
-        return this.$store.getters.getToken;
-      },
-      getRole() {
-        return this.$store.getters.getUser;
-      },
-    ...mapGetters(['getUser', 'getTokenString', ])
+<script>
+import { mapGetters, mapActions } from "vuex";
+export default {
+  name: "SiteLayout",
+  computed: {
+    getToken() {
+      return this.$store.getters.getToken;
     },
-    methods: {
-      ...mapActions(['logout'])
+    getRole() {
+      return this.$store.getters.getUser;
     },
-  };
-  </script>
-  
-  <style>
-  /* Estilos específicos del layout site */
-  </style>
-  
+    ...mapGetters(['getUser', 'getTokenString',])
+  },
+  methods: {
+    ...mapActions(['logout'])
+  },
+};
+</script>
+
+<style>
+/* Estilos específicos del layout site */
+</style>
--- src/views/Radio.vue
+++ src/views/Radio.vue
@@ -5,62 +5,59 @@
 
 
         <div class="container mx-auto p-4">
-            <div class="grid grid-cols-3 gap-4">
-                <!-- Columna más grande -->
-                <div class="col-span-2 text-white p-4 rounded" >
-                    <img src="../assets/Ahora-en-Vivo.png" class="bg-white mb-4" alt="Evento Aura"
-                        style="max-width: 100%; height: auto; ">
-                    <img src="../assets/Programacion1.jpg" alt="" class="mb-4">
+    <div class="grid grid-cols-1 lg:grid-cols-3 gap-4">
+        <!-- Columna más grande -->
+        <div class="lg:col-span-2 text-white p-4 rounded">
+            <img src="../assets/Ahora-en-Vivo.png" class="bg-white mb-4" alt="Evento Aura" style="max-width: 100%; height: auto;">
+            <img src="../assets/Programacion1.jpg" alt="" class="mb-4">
 
-                    <video controls crossorigin playsinline style="width: 100%;">
-                        <source type="application/x-mpegURL"
-                            src="https://live.armortemplate.com/stream/hls/playlist.m3u8">
-                    </video>
+            <video controls crossorigin playsinline style="width: 100%;">
+                <source type="application/x-mpegURL" src="https://live.armortemplate.com/stream/hls/playlist.m3u8">
+            </video>
 
-                    <!-- usuarios conectados -->
-                    <div class="bg-gray-800 p-4 rounded mt-4">
-                        <h2 class="text-xl font-bold">Usuarios Conectados {{ usuarios_conectados }}</h2>
-                    </div>
-                    <!-- Comments Section -->
-                    <Chat></Chat>
+            <!-- usuarios conectados -->
+            <div class="bg-gray-800 p-4 rounded mt-4">
+                <h2 class="text-xl font-bold">Usuarios Conectados {{ usuarios_conectados }}</h2>
+            </div>
 
-                </div>
-                <!-- Columna más pequeña -->
-                <div class="text-white p-4 rounded">
-                    <img src="../assets/slogan.png" alt="">
+            <!-- Comments Section -->
+            <Chat></Chat>
+        </div>
 
-                    <a href="https://aura.armortemplate.com/comprar">
-                        <img class="mt-4" src="../assets/FLAYER_1.jpg" alt="">
-                        <img class="mt-4 cursor-pointer" src="../assets/comprar-tickets.png" alt="">
-                    </a>
+        <!-- Columna más pequeña -->
+        <div class="text-white p-4 rounded">
+            <img src="../assets/slogan.png" alt="">
 
-                    <!-- patrocinadores, muy pronto! -->
-                    <div class=" p-4 rounded mt-4 hidden">
-                        <h2 class="text-xl font-bold">Patrocinadores </h2>
-                        <div class="mt-4 space-y-4">
+            <a href="https://aura.armortemplate.com/comprar">
+                <img class="mt-4" src="../assets/FLAYER_1.jpg" alt="">
+                <img class="mt-4 cursor-pointer" src="../assets/comprar-tickets.png" alt="">
+            </a>
 
-                            <div class="flex justify-between items-center bg-gray-700 p-2 rounded">
-                                <div class="flex items-start space-x-4">
-                                    <i class="fas fa-user-circle text-3xl mt-1"></i>
-                                    <div>
-                                        <h3 class="font-bold">Roberto Luis</h3>
-                                        <p class="text-sm text-gray-400">Hace 2 horas</p>
-                                        <!-- <p>Comentario del stream.</p> -->
-                                    </div>
-                                </div>
-                                <span>ARS $200</span>
+            <!-- patrocinadores, muy pronto! -->
+            <div class="p-4 rounded mt-4 hidden">
+                <h2 class="text-xl font-bold">Patrocinadores </h2>
+                <div class="mt-4 space-y-4">
+                    <div class="flex justify-between items-center bg-gray-700 p-2 rounded">
+                        <div class="flex items-start space-x-4">
+                            <i class="fas fa-user-circle text-3xl mt-1"></i>
+                            <div>
+                                <h3 class="font-bold">Roberto Luis</h3>
+                                <p class="text-sm text-gray-400">Hace 2 horas</p>
+                                <!-- <p>Comentario del stream.</p> -->
                             </div>
-
-                            <!-- login -->
-                            <Entrar></Entrar>
-                            <button class="w-full bg-green-500 p-2 rounded mt-4" v-if="getToken">Patrocinar</button>
-
-
                         </div>
+                        <span>ARS $200</span>
                     </div>
+
+                    <!-- login -->
+                    <Entrar></Entrar>
+                    <button class="w-full bg-green-500 p-2 rounded mt-4" v-if="getToken">Patrocinar</button>
                 </div>
             </div>
         </div>
+    </div>
+</div>
+
 
 
 
